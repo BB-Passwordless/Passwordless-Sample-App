9aa1c435b535c8679129ed93dc4290731f9250a3
Nitesh Singh <nitesh@mollatech.com>
2021-12-13 12:56:28 +0530

    Spring Boot Sample App added

---
+++ passwordless-spring-boot-Sample-App/src/main/java/com/passwordless/PasswordlessSampleApp/PasswordlessSampleAppApplication.java
@@ -0,0 +1,78 @@
+package com.passwordless.PasswordlessSampleApp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.sun.xml.internal.ws.api.message.Header;
+import jdk.nashorn.internal.ir.ObjectNode;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+@Controller
+@SpringBootApplication
+public class PasswordlessSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordlessSampleAppApplication.class, args);
+	}
+
+
+	@RequestMapping(value={"/","/login"})
+	public String login(Model model){
+		HashMap<String,String> data = new HashMap<>();
+		data.put("type","Login");
+		model.addAttribute("data",data);
+		return "index";
+	}
+
+	@RequestMapping(value="/register")
+	public String register(Model model){
+		HashMap<String,String> data = new HashMap<>();
+		data.put("type","Register");
+		model.addAttribute("data",data);
+		return "index";
+	}
+	@RequestMapping(value = "/register-success")
+	public String registerSuccess(){
+
+		return "registerSuccess";
+	}
+	@RequestMapping(value = "/login-success")
+	public String loginSuccess(){
+
+		return "success";
+	}
+	@RequestMapping(value = "/authenticateToken/{accessToken}")
+	public String approveToken(@PathVariable String accessToken, Model model) throws JsonProcessingException {
+
+		String url = "https://home.passwordless.com.au/api/verifyToken";
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		HashMap<String, String> map = new HashMap<>();
+		map.put("accessToken",accessToken);
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			Object result = restTemplate.postForObject(url,map,Object.class);
+			System.out.println(result);
+			model.addAttribute("data",result);
+			return "registerDetails";
+		} catch (RuntimeException exception) {
+			System.out.println(exception);
+			return "error";
+		}
+
+
+	}
+}
---
+++ passwordless-spring-boot-Sample-App/src/main/resources/static/js/script.js
@@ -0,0 +1,240 @@
+const fido = new passwordless(
+    "BASE_URL",
+    "CLIENT_ID",
+);
+
+function randomString(length, chars) {
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+const generateRandomId = () => {
+  const rString = randomString(
+      32,
+      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  );
+  return rString;
+};
+
+const registerFun = async () => {
+  //console.log("regn session id",sessionId);
+  const qrImg = document.getElementById("qrImg");
+  qrImg.src = "#";
+
+  const username = this.username.value;
+  console.log(username);
+  if (this.authMethod.value == 1) {
+    fido
+        .register({ username })
+        .then(async (response) => {
+          console.log(response);
+          if (response.verified) {
+            await AddToAudit(response.userId, 1, "success");
+            window.location.href = "/register-success";
+          } else await AddToAudit(response.userId, 1, "error");
+        })
+        .catch(async (error) => {
+          alert(error);
+        });
+  } else if (this.authMethod.value == 2) {
+    generateQR(username, 1, "web");
+  } else if (this.authMethod.value == 3) {
+    generateQR(username, 1, "app");
+
+  }
+
+};
+
+const loginFun = async () => {
+  //console.log("login session id",sessionId);
+  const username = this.username.value;
+  const qrImg = document.getElementById("qrImg");
+  qrImg.src = "#";
+
+  if (this.authMethod.value == 1) {
+    fido.login({ username })
+        .then(async (response) => {
+          if (response.verified) {
+            await AddToAudit(response.userId, 2, "success");
+            window.location.href = "/login-success";
+          } else await AddToAudit(response.userId, 2, "error");
+        })
+        .catch(async (error) => {
+          alert(error);
+        });
+  } else if (this.authMethod.value == 2) {
+    generateQR(username, 2, "web");
+  } else if (this.authMethod.value == 3) {
+    generateQR(username, 2, "app");
+
+  }
+};
+
+const generateQR = async (username, type, platform = "web") => {
+  const qrImg = document.getElementById("qrImg");
+  qrImg.src = "#";
+  const success = async (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const ua = detect.parse(navigator.userAgent);
+    const reqTime = new Date().toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    let path;
+
+    if (type == 1) path = `${location.origin}/registerToken`;
+    else if (type == 2) path = `${location.origin}/loginToken`;
+    else path = `${location.origin}/addDevice`;
+
+    const userDetails = {
+      latitude,
+      longitude,
+      device: `${ua.os.name},${ua.browser.name}`,
+      username,
+      type,
+      platform,
+      reqTime,
+      path,
+    };
+    //console.log(userDetails);
+
+    fido
+        .generateQR(userDetails)
+        .then(async (response) => {
+          //console.log(response);
+          qrImg.src = response.url;
+
+          console.log({ accessToken: response.accessToken });
+
+          if (type === 2) $("#modal").modal("show");
+          else $("#modal").modal("show");
+
+          console.log(userDetails);
+
+          const transactionResponse = await fido.getTransactionStatusOnChange(
+              userDetails.id
+          );
+          if (transactionResponse.status == "SUCCESS") {
+            console.log("transaction success");
+            if (type == 1) window.location.href = "/register-success";
+            else if (type == 2) window.location.href = "/login-success";
+            else if (type == 3) window.location.href = "/addDeviceSuccess";
+          } else {
+            console.log("Something went wrong");
+          }
+        })
+        .catch((error) => alert(error));
+  };
+
+  function error() {
+    alert("Unable to retrieve your location");
+  }
+
+  if (!navigator.geolocation) {
+    alert("Geolocation is not supported by your browser");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+    });
+  }
+};
+
+const addDevice = async (sessionId) => {
+  //console.log("regn session id",sessionId);
+  const qrImg = document.getElementById("qrImg");
+  qrImg.src = "#";
+
+  const username = this.username.value;
+  //console.log(username);
+  if (this.authMethod.value == 1) {
+    fido
+        .addDevice(username)
+        .then(async (response) => {
+          if (response.verified) {
+            await AddToAudit(response.userId, 3, "success");
+
+            alert("new device added successfully");
+          } else await AddToAudit(response.userId, 3, "error");
+        })
+        .catch(async (error) => {
+          alert(error);
+        });
+  } else if (this.authMethod.value == 2) {
+    generateQR(username, 3, "web", sessionId);
+  } else if (this.authMethod.value == 3) {
+    generateQR(username, 3, "app", sessionId);
+    $("#RegisterModal").modal("show");
+  } else alert("not done yet");
+};
+
+const AddToAudit = async (userId, type, label) => {
+  const ispAPI = await fetch("https://ipapi.co/json");
+
+  const data = await ispAPI.json();
+  const ua = detect.parse(navigator.userAgent);
+  data.time = new Date();
+  (data.browser = ua.browser.name), (data.device = ua.os.name);
+  data.label = label;
+  fido.Audit({ userId, data, type });
+};
+
+const approveRegister = () => {
+  const { username, id } = document.getElementById("user-data").dataset;
+  fido
+      .register({ username, id })
+      .then(async (response) => {
+        console.log(response);
+        if (response.verified) {
+          await AddToAudit(response.userId, 1, "success");
+          window.location.href = "/register-success";
+        } else await AddToAudit(response.userId, 1, "error");
+      })
+      .catch(async (error) => {
+        alert(error);
+      });
+};
+
+const approveLogin = () => {
+  const { username, id } = document.getElementById("user-data").dataset;
+  console.log(username, id);
+
+  fido
+      .login({ username, id })
+      .then(async (response) => {
+        console.log("loginResponse", response);
+        if (response.verified) {
+          await AddToAudit(response.userId, 2, "success");
+          window.location.href = "/login-success";
+        } else await AddToAudit(response.userId, 2, "error");
+      })
+      .catch(async (error) => {
+        alert(error);
+      });
+};
+
+const declineProcess = (process) => {
+  window.close();
+};
+
+const approveDevice = (username, id) => {
+  fido
+      .addDevice({ username, id })
+      .then(async (response) => {
+        console.log(response);
+        if (response.verified) {
+          await AddToAudit(response.userId, 3, "success");
+          alert("device added successfully");
+          window.close();
+        } else await AddToAudit(response.userId, 3, "error");
+      })
+      .catch(async (error) => {
+        alert(error);
+      });
+};
